befbb95d5e5cee9bc985d964d7c56c9343b4e3af
MaksymMykhailets <149175463+MaksymMykhailets@users.noreply.github.com>
2024-10-13 02:41:04 +0200

    Update campersService.js

--- src/services/campersService.js
+++ src/services/campersService.js
@@ -3,8 +3,15 @@ import axios from 'axios';
 const BASE_URL = 'https://66b1f8e71ca8ad33d4f5f63e.mockapi.io/campers';
 
 export const fetchCampers = async (filters) => {
-  const response = await axios.get(BASE_URL, { params: filters });
-  return response.data;
+  try {
+    const response = await axios.get(BASE_URL, { params: filters });
+    return response.data;
+  } catch (error) {
+    if (error.response && error.response.status === 404) {
+      throw new Error('No campers found with the specified filters.');
+    }
+    throw new Error('Something went wrong.');
+  }
 };
 
 export const fetchCamperById = async (id) => {
